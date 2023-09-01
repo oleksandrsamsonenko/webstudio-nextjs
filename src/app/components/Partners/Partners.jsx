6377798103f8ad33b759f89f8cf2f4ea44f165a6
Oleksandr Samsonenko <invizibji@gmail.com>
2023-09-01 21:09:15 +0300

    keys added in partners component

--- src/app/components/Partners/Partners.jsx
+++ src/app/components/Partners/Partners.jsx
@@ -2,12 +2,12 @@ import styles from './Partners.module.scss';
 import { Logo1, Logo2, Logo3, Logo4, Logo5, Logo6 } from './icons';
 
 const logosList = [
-  <Logo1 styleProp={styles.logo} />,
-  <Logo2 styleProp={styles.logo} />,
-  <Logo3 styleProp={styles.logo} />,
-  <Logo4 styleProp={styles.logo} />,
-  <Logo5 styleProp={styles.logo} />,
-  <Logo6 styleProp={styles.logo} />,
+  { id: 1, image: <Logo1 styleProp={styles.logo} /> },
+  { id: 2, image: <Logo2 styleProp={styles.logo} /> },
+  { id: 3, image: <Logo3 styleProp={styles.logo} /> },
+  { id: 4, image: <Logo4 styleProp={styles.logo} /> },
+  { id: 5, image: <Logo5 styleProp={styles.logo} /> },
+  { id: 6, image: <Logo6 styleProp={styles.logo} /> },
 ];
 
 export default function Partners() {
@@ -17,7 +17,9 @@ export default function Partners() {
         <h2 className={styles.title}>Наші клієнти</h2>
         <ul className={styles.list}>
           {logosList.map(item => (
-            <li className={styles.item}>{item}</li>
+            <li className={styles.item} key={item.id}>
+              {item.image}
+            </li>
           ))}
         </ul>
       </div>
